c44b23e07a6e0fec972b2604bf3e5136ff1801a2
Mark Reilly <mreilly@kbsp.com>
2015-04-16 08:59:56 -0400

    Renamed AELabelSettings.js to AESettings.js
    Changed AELabelSettings.js to AESettings.js and added configuration
    variables for all the other options

---
+++ AESettings.js
@@ -0,0 +1,42 @@
+// Sketch Plugin: AEFlowchart (settings)
+// Source: github.com/tadija/AEFlowchart
+// Version: 1.0
+
+// Label Variables
+var labelFontName = "Helvetica";
+var labelFontSize = 12;
+var labelFontColor = "#FFFFFF";
+var labelDropShadow = false;
+
+// Connect Variables
+var connectLineColor = "#666766";
+
+// Decision Variables
+var decisionColorStart = "#FF552A";
+var decisionColorEnd = decisionColorStart; // 'Flat' Gradient
+var decisionInnerShadows = false;
+var decisionFontColor = labelFontColor;
+
+// inputOutput Variables
+var inputOutputColorStart = "#B4EC51";
+var inputOutputColorEnd = "#429321";
+var inputOutputInnerShadows = false;
+var inputOutputFontColor = labelFontColor;
+
+// LabelLine Variables
+var labelLineColorStart = "#FFFFFF";
+var labelLineColorEnd = labelLineColorStart; // 'Flat' Gradient
+var labelLineInnerShadows = false;
+var labelLineFontColor = "#262626";
+
+// Process Variables
+var processColorStart = "#1C69D4";
+var processColorEnd = processColorStart;  // 'Flat' Gradient
+var processInnerShadows = false;
+var processFontColor = labelFontColor;
+
+// startEnd Variables
+var startEndColorStart = "#1F72E4";
+var startEndColorEnd =  "#1A62C5";
+var startEndInnerShadows = false;
+var startEndFontColor = labelFontColor;
